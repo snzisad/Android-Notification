8b84f114bc556a957b89cb34b6cbdb1aa4520437
snzisad <snzisad@gmail.com>
2018-03-21 15:08:12 +0600

    Update

--- app/src/main/java/zisad/android/com/notification/MainActivity.java
+++ app/src/main/java/zisad/android/com/notification/MainActivity.java
@@ -18,14 +18,16 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     NotificationCompat.Builder n;
     public static final int UID = 1009;
-    String notify;
     SharedPreferences.Editor sharedEditor;
     SharedPreferences sharedPreferences;
+
+    Button currentNotificationButton, laterNotificationButton;
 
 
     @Override
@@ -36,23 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("Notification",Context.MODE_PRIVATE);
         sharedEditor = sharedPreferences.edit();
-        notify=sharedPreferences.getString("Zisad",null);
 
+        currentNotificationButton=(Button)findViewById(R.id.currentnotification);
+        laterNotificationButton=(Button)findViewById(R.id.laternotification);
 
-        notificationcheck();
-    }
-
-    public void notificationcheck(){
-
-        if(notify=="Yes"){
-            notification();
-        }
-        else if(notify=="No"){
-
-        }
-        else{
-            permission();
-        }
+        currentNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permission();
+            }
+        });
     }
 
 
@@ -85,16 +80,7 @@ public class MainActivity extends AppCompatActivity {
         alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        sharedEditor.putString("Zisad","Yes");
                         notification();
-                    }
-                }
-        );
-
-        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        sharedEditor.putString("Zisad","No");
                     }
                 }
         );
